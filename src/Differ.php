a570e7caed0300763d7c823447d3ee02f5108e8c
igor.pvdc <igor.pvdc@gmail.com>
2021-12-18 14:35:36 +0300

    added file1.json and file2.json, updated Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -2,35 +2,18 @@
 
 namespace Differentiator\Differ;
 
-$file1 = '{
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-}';
-
-$file2 = '{
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-}';
-
-$file3 = '/home/igor/php-project-lvl2/src/file1.json';
-
-function jsonDecode($file)
+function genDiff($file1, $file2)
 {
-    return json_decode($file, true);
-}
+    if (file_exists("/home/igor/php-project-lvl2/{$file1}")) {
+        $str1 = file_get_contents("/home/igor/php-project-lvl2/{$file1}");
+    }
+    if (file_exists("/home/igor/php-project-lvl2/{$file2}")) {
+        $str2 = file_get_contents("/home/igor/php-project-lvl2/{$file2}");
+    }
 
-$array = jsonDecode($file3);
+    $array1 = json_decode($str1, true);
 
-var_dump($array);
-
-function gendiff($file1, $file2)
-{
-    $array1 = json_decode($file1, true);
-
-    $array2 = json_decode($file2, true);
+    $array2 = json_decode($str2, true);
 
     $newArray = [];
 
@@ -56,6 +39,6 @@ function gendiff($file1, $file2)
     return $newArray;
 }
 
-$array = gendiff($file1, $file2);
+$array = genDiff('file1.json', 'file2.json');
 
 var_dump(json_encode($array));
