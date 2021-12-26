d0a37576a06936ccda3c84578a3f59775b352f16
igor.pvdc <igor.pvdc@gmail.com>
2021-12-26 16:05:24 +0300

    updated Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -6,50 +6,86 @@ use function Functional\sort;
 
 function genDiff($file1, $file2)
 {
-    $jsonData1 = json_decode(pathToFile($file1), true);
-    $jsonData2 = json_decode(pathToFile($file2), true);
+
+    $jsonData1 = json_decode(getPathToFile($file1), true);
+    $jsonData2 = json_decode(getPathToFile($file2), true);
     $diffBetweenFiles = [];
 
-    foreach ($jsonData1 as $key => $value) {
-        if (array_key_exists($key, $jsonData2)) {
-            if ($value === $jsonData2[$key]) {
-                $diffBetweenFiles["  {$key}"] = $value;
-            } else {
-                $diffBetweenFiles["- {$key}"] = $value;
-                $diffBetweenFiles["+ {$key}"] = $jsonData2[$key];
-            }
+    $uniqueKeys = array_keys(array_merge($jsonData1, $jsonData2));
+
+    $sortedKeys = sort($uniqueKeys, fn ($left, $right) => strcmp($left, $right), true);
+
+    $keysInfo = array_map(function ($key) use ($jsonData1, $jsonData2) {
+        if (array_key_exists($key, $jsonData1) && !array_key_exists($key, $jsonData2)) {
+            $value = isBoll($jsonData1[$key]);
+            return [
+                'key' => $key,
+                'status' => 'deleted',
+                'value' => $value,
+            ];
+        }
+
+        if (array_key_exists($key, $jsonData2) && !array_key_exists($key, $jsonData1)) {
+            $value = isBoll($jsonData2[$key]);
+            return [
+                'key' => $key,
+                'status' => 'added',
+                'value' => $value,
+            ];
+        }
+
+        if ($jsonData1[$key] === $jsonData2[$key]) {
+            $value = isBoll($jsonData1[$key]);
+            return [
+                'key' => $key,
+                'status' => 'same',
+                'value' => $value,
+            ];
+        }
+
+        $value1 = isBoll($jsonData1[$key]);
+        $value2 = isBoll($jsonData2[$key]);
+
+        return [
+            'key' => $key,
+            'status' => 'different',
+            'value1' => $value1,
+            'value2' => $value2,
+        ];
+    }, $sortedKeys);
+
+    foreach ($keysInfo as $key) {
+        if ($key['status'] === 'same') {
+            $diffBetweenFiles[] = "  {$key['key']}: {$key['value']}";
+        } elseif ($key['status'] === 'deleted') {
+            $diffBetweenFiles[] = "- {$key['key']}: {$key['value']}";
+        } elseif ($key['status'] === 'added') {
+            $diffBetweenFiles[] = "+ {$key['key']}: {$key['value']}";
         } else {
-            $diffBetweenFiles["- {$key}"] = $value;
+            $diffBetweenFiles[] = "- {$key['key']}: {$key['value1']}";
+            $diffBetweenFiles[] = "+ {$key['key']}: {$key['value2']}";
         }
     }
 
-    foreach ($jsonData2 as $key => $value) {
-        if (!array_key_exists("  {$key}", $diffBetweenFiles)) {
-            $diffBetweenFiles["+ {$key}"] = $value;
-        }
-    }
-
-    sort($collection, fn ($left, $right) => strcmp($left, $right));
-
-    return json_encode($diffBetweenFiles);
+    return implode("\n", $diffBetweenFiles);
 }
 
 
-function pathToFile($fileName): string
+function getPathToFile($fileName): string
 {
-    $path1 = __DIR__ . "/{$fileName}";
-    $path2 = "../{$fileName}";
-    $path3 = "./{$fileName}";
-
-    if (file_exists($path1)) {
-        return file_get_contents($path1);
-    } elseif (file_exists($path2)) {
-        return file_get_contents($path2);
+    if (!file_exists($fileName)) {
+        throw new \Exception('error');
     }
-    return file_get_contents($path3);
+
+    return file_get_contents($fileName);
 }
 
-$result = genDiff('file1.json', 'file2.json');
-
-var_dump($result);
-
+function isBoll($value): string
+{
+    if ($value === true) {
+        return 'true';
+    } elseif ($value === false) {
+        return 'false';
+    }
+    return $value;
+}
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Differentiator\Differ\Tests;
+namespace Differentiator\Tests;
 
 use PHPUnit\Framework\TestCase;
 use function Differentiator\Differ\genDiff;
@@ -15,12 +15,15 @@ class DifferTest extends TestCase
 
     public function test_genDiff(): void
     {
-        $diff = $this->getFixtureFullPath('DiffBetweenTestsFiles.json');
+        $file1 = $this->getFixtureFullPath('file1.json');
+        $file2 = $this->getFixtureFullPath('file2.json');
+
+        $diff = $this->getFixtureFullPath('DiffBetweenTestsFiles.txt');
 
         $expected = file_get_contents($diff);
 
-        $resultDiff = genDiff('file1.json', 'file2.json');
+        $resultDiff = genDiff($file1, $file2);
 
-        $this ->assertEquals($resultDiff, $expected);
+        $this->assertEquals($resultDiff, $expected);
     }
 }
